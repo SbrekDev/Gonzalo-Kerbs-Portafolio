c9e6dff11b90c1f32ae3b76ef8c5b4cb5c98d4f6
Gonzalo Kerbs <gonzakerbs@gmail.com>
2024-12-09 16:13:59 -0300

    skills components completed

--- src/components/SkillsComponent.tsx
+++ src/components/SkillsComponent.tsx
@@ -35,13 +35,46 @@ const SkillsComponent = () => {
   ];
 
   const certificaciones = [
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
-    ["Responsive Web Design", "/cv/Responsive Web Design Certificado.pdf"],
+    [
+      "Responsive Web Design",
+      "https://coursera.org/share/43a593f1f8dfadc568db1899592a6d41",
+    ],
+    [
+      "Bootstrap 4",
+      "https://coursera.org/share/7a64b1cccc0b596857464f41464f02f8",
+    ],
+    [
+      "HTML5 | CSS3 | JS | PHP | MYSQL",
+      "https://www.udemy.com/certificate/UC-7e77a4fb-ce0a-4c64-a4fc-b8049cea5a6a/",
+    ],
+    [
+      "JavaScript Moderno | Testing",
+      "https://www.udemy.com/certificate/UC-158e8998-f260-4841-a727-9fefe8220689/",
+    ],
+    [
+      "Node JS | Express | MVC",
+      "https://www.udemy.com/certificate/UC-20ae2b2d-5fb3-4c3a-b782-105d8efc02ac/",
+    ],
+    [
+      "React | TypeScript",
+      "https://www.udemy.com/certificate/UC-4c4e6275-86fc-432a-bc50-763eeb54e1ce/",
+    ],
+    [
+      "React | NextJS | MongoDB | GraphQL",
+      "https://www.udemy.com/certificate/UC-95b282e0-2860-4996-9007-531930751444/",
+    ],
+    [
+      "React Native",
+      "https://www.udemy.com/certificate/UC-9ac6cb32-22e6-46b9-8dac-9b5f8b90f595/",
+    ],
+    [
+      "Vue | Firebase | MongoDB",
+      "https://www.udemy.com/certificate/UC-52a94e0d-d57c-49f1-80d7-a1054df74fdf/",
+    ],
+    [
+      "Responsive Web Design",
+      "https://www.freecodecamp.org/certification/GonzaloKerbs/responsive-web-design",
+    ],
   ];
 
   return (
@@ -56,21 +89,19 @@ const SkillsComponent = () => {
               <div className="flex flex-col justify-center items-center gap-5 p-5 mx-3 shadow-2xl rounded-md">
                 <h3 className="text-3xl text-center mb-3">PRESENTACIÓN</h3>
                 <p className="text-center font-extralight">
-                  Desarrollador web autodidacta, apasionado por la{" "}
+                  Desarrollador Fullstack autodidacta, apasionado por la{" "}
                   <strong className="text-accent">
                     informática y el diseño
                   </strong>
                   . Proveniente de Argentina.
                 </p>
                 <p className="text-center font-extralight">
-                  Llevo un año especializándome en el área del Front-end,
-                  aprendiendo{" "}
+                  Llevo dos años especializándome en el área del Front-end y
+                  Back-end, aprendiendo{" "}
                   <strong className="text-accent">nuevos recursos</strong> a
                   diario para{" "}
-                  <strong className="text-accent">
-                    mejorar la calidad de mis servicios
-                  </strong>
-                  .
+                  <strong className="text-accent">mejorar la calidad </strong>
+                  de mis servicios.
                 </p>
                 <a
                   className="text-center bg-accent px-5 py-3 rounded-md shadow-md hover:bg-secondary hover:text-accent"
@@ -83,7 +114,7 @@ const SkillsComponent = () => {
             </div>
             <div className="mb-5 flex flex-col justify-center items-center gap-5 p-6 mt-3  py-5 mx-3 shadow-2xl rounded-md">
               <h3 className="text-3xl text-center mb-5">APTITUDES</h3>
-              <div className="grid gap-6 font-extralight text-xl">
+              <div className="flex flex-col gap-6 w-full font-extralight text-xl">
                 <p>
                   <strong className="text-accent accent-shadow mr-2">⬤</strong>
                   Disciplina
@@ -102,7 +133,7 @@ const SkillsComponent = () => {
                 </p>
                 <p>
                   <strong className="text-accent accent-shadow mr-2">⬤</strong>
-                  Disposición
+                  Disposición Constante
                 </p>
                 <p>
                   <strong className="text-accent accent-shadow mr-2">⬤</strong>
@@ -159,9 +190,9 @@ const SkillsComponent = () => {
               <div className="grid grid-cols-2 gap-3">
                 {certificaciones.map((certificacion) => (
                   <a
+					target="_blank"
                     className="bg-gradient-to-b from-transparent to-secondary/70 rounded-md p-3 flex flex-col justify-center items-center gap-2"
                     href={certificacion[1]}
-                    download={certificacion[0]}
                   >
                     <i className="fa-solid fa-graduation-cap text-3xl text-accent" />
                     <p className="text-center font-extralight">
